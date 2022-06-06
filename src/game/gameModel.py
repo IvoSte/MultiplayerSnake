581c0de28fa7a52a3c65674faf8d1d24ede79818
IvoSte <36919251+IvoSte@users.noreply.github.com>
2022-06-06 14:00:38 +0200

    started work on the game model

--- src/game/gameModel.py
+++ src/game/gameModel.py
@@ -1,14 +1,19 @@
 from state import State
 
 
-class GameState:
+class GameModel:
     def __init__(self):
-        self.state: State
+        self.players
+        # self.snakes
+        # self.entities
+        # self.food
+        self.environment
 
 
 # What do I want in the game state?
 # The game state should be the general handler to know all information about the game
 # Like all players' positions, positions of food,
+# so, it has all entities. 
 # but also be able to derive / update information about the game like
 # How many times which player has won.
 # It should also be able to package itsself in a tiny format to be sent over the
